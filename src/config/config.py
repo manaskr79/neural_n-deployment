ee4d8bec8e685f4737f9c67d39a7ec0cd9fd6cb0
manaskr79 <2022btechaidsmanas12819@poornima.edu.in>
2024-05-29 12:27:33 +0530

    Added config package along with it's config module and created preprocessing package

---
+++ src/config/config.py
@@ -0,0 +1,22 @@
+NUM_INPUTS = 2
+NUM_LAYERS = 3
+P = [NUM_INPUTS,2,1]
+
+f = [None,"Linear","Sigmoid"]
+
+LOSS_FUNCTION = "Mean Squared Error"
+MINI_BATCH_SIZE = 1
+#theta0 = [None]
+#theta = [None]
+
+#[None]* NUM_LAYERS
+#[None]* NUM_LAYERS
+
+"""
+fl_by_del_z = [None]*NUM_LAYERS
+hl_by_del_theta0 = [None]*NUM_LAYERS
+hl_by_del_theta = [None]*NUM_LAYERS
+L_by_del_h = [None]*NUM_LAYERS
+L_by_del_theta0 = [None]*NUM_LAYERS
+L_by_del_theta = [None]*NUM_LAYERS
+"""
